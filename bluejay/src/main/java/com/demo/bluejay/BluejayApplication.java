34db8f0bc3e709e2576abef00526f64bcdcf409b
Sonali Mathur <sonalimathur@Sonalis-MacBook-Air.local>
2023-03-15 09:40:24 +0530

    First

--- bluejay/src/main/java/com/demo/bluejay/BluejayApplication.java
+++ bluejay/src/main/java/com/demo/bluejay/BluejayApplication.java
@@ -3,13 +3,20 @@ package com.demo.bluejay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
-public class BluejayApplication {
+public class BluejayApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BluejayApplication.class, args);
 	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(BluejayApplication.class);
+	    }
 
 }
